11f5b9da03aa1ca1729af5ae682028d7b062d4a3
steelbridge-io <chris@steelbridge.io>
2025-12-10 02:16:28 -0800

    Add support for comments in single post template

--- single.php
+++ single.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * The template for displaying all single posts
@@ -8,12 +9,19 @@
  */
 
 get_header(); ?>
-    <div id="main">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h1 class="page-title"><?php the_title(); ?></h1>
-            <div class="page-content">
-                <?php the_content(); ?>
-            </div>
-        <?php endwhile; endif; ?>
-    </div>
+<div id="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1 class="page-title"><?php the_title(); ?></h1>
+        <div class="page-content">
+            <?php the_content(); ?>
+        </div>
+
+        <?php
+        // If comments are open or there are comments, load the comment template
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+        ?>
+    <?php endwhile; endif; ?>
+</div>
 <?php get_footer();
